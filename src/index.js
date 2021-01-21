def30fbc2b8aed807d8cbf8f6b6d63cd0971e1b0
Mitchell Rodríguez <mitrodle@gmail.com>
2021-01-21 00:33:34 -0500

    Add: State - Events With Class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import HolaMundo from './components/HolaMundo';
+// import App from './App';
+// import HolaMundo from './components/HolaMundo';
 import Button from './components/Button';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HolaMundo />
+    {/* <HolaMundo /> */}
     <Button text="Click" />
   </React.StrictMode>,
   document.getElementById('root')
